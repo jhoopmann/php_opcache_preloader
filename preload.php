1c183c08ddf16efd096051f3591157dacb5568f0
Jonas Hoopmann <jhoopmann@sportimport.de>
2019-12-02 11:57:13 +0100

    ff

--- preload.php
+++ preload.php
@@ -1,43 +1,66 @@
 <?php
 
-$roots = include 'root.php';
+$opts = include 'root.php';
 
 /**
  * @param $path
  */
-function __preload($path) {
-    if(is_file($path)) {
-        if(substr(
-                $path,
-                strlen($path) - 4,
-                4
-            ) === '.php'
-        ) {
-            if(!opcache_compile_file($path)) {
-                error_log('[COMPILE ERR ' . $path. ']');
-            } else {
-                error_log('[COMPILED ' . $path . ']');
-            }
-        }
-    } else if(is_dir($path)) {
-        $d = opendir($path);
-        while(($dpath = readdir($d)) !== false) {
-            if($dpath !== '..' &&
-                $dpath !== '.'
+function __preload(
+    $path,
+    $except =  []
+) {
+    if(!in_array(
+            $path,
+            $except
+        )
+    ) {
+        if(is_file($path)) {
+            if(substr(
+                    $path,
+                    strlen($path) - 4,
+                    4
+                ) === '.php'
             ) {
-                __preload($path . DIRECTORY_SEPARATOR . $dpath);
+                error_log('[COMPILE ' . $path . ']');
+
+                require_once $path;
+
+
             }
+        } else if(is_dir($path)) {
+            $d = opendir($path);
+            while(($dpath = readdir($d)) !== false) {
+                if($dpath !== '..' &&
+                    $dpath !== '.'
+                ) {
+                    __preload(
+                        $path . DIRECTORY_SEPARATOR . $dpath,
+                        $except
+                    );
+                }
+            }
+            closedir($d);
+        } else {
+            error_log('[UNKOWN FS NODE ' . $path . ']');
         }
-        closedir($d);
-    } else {
-        error_log('[UNKOWN FS NODE ' . $path . ']');
     }
 }
 
-foreach($roots as $root)  {
-    $root = realpath($root);
+foreach($opts['include'] as $include) {
+    error_log('[INIT PRECOMPILER INCLUDE ' . $include . ']');
 
+    require_once $include;
+}
+
+foreach($opts['directory'] as $root)  {
     error_log('[INIT PRECOMPILING FOR PATH ' . $root . ']');
 
-    __preload($root);
+    __preload(
+        $root,
+        [
+            ...$opts['except'],
+            ...$opts['include'],
+            (__DIR__ . DIRECTORY_SEPARATOR . 'preload.php')
+        ]
+    );
 }
--- root.php
+++ root.php
@@ -1,6 +1,17 @@
 <?php
 
 return [
-    __DIR__ . DIRECTORY_SEPARATOR . 'html' .
-        DIRECTORY_SEPARATOR . 'warehouse.sportimport.de'
+    'include' => [
+        realpath(__DIR__ . '/../warehouse.sportimport.de/php/autoload.php')
+    ],
+    'except' => [
+        '/home/jhoopmann/projects/warehouse.sportimport.de/php/SportImport/Warehouse/Runnable/events.php'
+    ],
+    'directory' => [
+        realpath(
+            __DIR__ . DIRECTORY_SEPARATOR . '..' .
+                DIRECTORY_SEPARATOR . 'warehouse.sportimport.de' . 
+                DIRECTORY_SEPARATOR . 'php/SportImport'
+        )
+    ]
 ];
